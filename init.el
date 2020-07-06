7ad8a0afdec7c617564f96065ee1cd197394a2f0
GiulioCentorame <giulio.centorame@outlook.it>
2020-07-06 16:19:32 +0100

    Deactivate unused modules (for now)

--- init.el
+++ init.el
@@ -143,9 +143,7 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org               ; organize your plain life in plain text
-        +brain
-        +roam
-        +journal)
+        +roam)
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
