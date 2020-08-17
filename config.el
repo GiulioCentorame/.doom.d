f703e2b1cf2721205b67c8d550f22e15477a6ed9
GiulioCentorame <giulio.centorame@outlook.it>
2020-08-17 12:33:43 +0100

    Add support for Wakatime

--- config.el
+++ config.el
@@ -347,3 +347,29 @@
 ;;     (interactive)
 ;;     (org-roam-server-mode 1)
 ;;     (browse-url-xdg-open (format "http://localhost:%d" org-roam-server-port))))
+
+;; Wakatime ;;
+;;;;;;;;;;;;;;
+(use-package! wakatime-mode
+  :ensure t)
+
+;; Enables wt for all buffers
+(global-wakatime-mode)
+
+;; Remember to set `wakatime-api-key' and `wakatime-cli-path'
+;; OR to keep your config.el clean of personal information, run on CLI
+;; `wakatime --config-write api_key [WAKATIME_API_KEY]'
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (wakatime-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
