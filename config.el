3df2a4209f9f8f3b8c8b689afbe45f9dc3d15abf
GiulioCentorame <giulio.centorame@outlook.it>
2020-08-27 12:25:50 +0100

    Aesthetic changes

--- config.el
+++ config.el
@@ -24,12 +24,18 @@
       doom-variable-pitch-font (font-spec :family "sans" :size 17)
       doom-big-font (font-spec :family "monospace" :size 30))
 
+(defun my-buffer-face-mode-variable ()
+  "Set font to a variable width (proportional) fonts in current buffer"
+  (interactive)
+  (setq buffer-face-mode-face '(:family "monospace" :height 100 ))
+  (buffer-face-mode))
+(add-hook 'org-mode-hook 'my-buffer-face-mode-variable)
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-;; (setq doom-theme 'doom-dracula)     ;;this is another good one
+;; (setq doom-theme 'doom-one)
+(setq doom-theme 'doom-dracula)     ;;this is another good one
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
